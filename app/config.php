288740e0b32bae0dd2f0afaff868ab2c1a04a921
bintangprakasa22 <101088377+bintangprakasa22@users.noreply.github.com>
2026-04-22 16:35:48 +0700

    Add files via upload

---
+++ app/config.php
@@ -0,0 +1,10 @@
+<?php
+// Konfigurasi Database
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'organisasi_uts');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+
+// Konfigurasi URL Dasar (Base URL)
+// Sesuaikan dengan nama folder di htdocs kamu
+define('BASEURL', 'http://localhost/organisasi_uts');
